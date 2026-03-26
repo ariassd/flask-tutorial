e9433e933fb5dbf25ab39d943db9f78d5d096511
Luis Arias <ariassd@gmail.com>
2026-03-26 15:17:14 -0600

    Modules

--- user/routes.py
+++ user/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 
 user_bp = Blueprint('user', __name__, template_folder='templates')
 
@@ -11,3 +11,22 @@ def user(name, last_name):
         'shopping_list': shopping_list,
     }
     return render_template('/user/hello.html', **context)
+
+
+@user_bp.route('/edit', methods=['GET'])
+def edit():
+    # In a real application, you would fetch the user from a database
+    # For this example, we'll use a mock user
+    user = {'username': 'john_doe', 'email': 'john@example.com'}
+    return render_template('user/edit.html', user=user)
+
+
+@user_bp.route('/update', methods=['POST'])
+def update():
+    # Handle the form submission here
+    # In a real application, you would update the user in a database
+    username = request.form['username']
+    email = request.form['email']
+
+    # For this example, we'll just redirect back to the home page
+    return redirect(url_for('user.index'))
